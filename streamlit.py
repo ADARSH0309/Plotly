3c27c1724bf1b28c3c4af30eefa6d66aa43df1e4
ADARSH0309 <156338073+ADARSH0309@users.noreply.github.com>
2025-02-28 09:52:22 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+
+
+import plotly.graph_objects as go
+import numpy as np
+X, Y, Z = np.mgrid[-8:8:40j, -8:8:40j, -8:8:40j]
+values = np.sin(X*Y*Z) / (X*Y*Z)
+
+fig = go.Figure(data=go.Volume(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=0.1,
+    isomax=0.8,
+    opacity=0.1, # needs to be small to see through all surfaces
+    surface_count=17, # needs to be a large number for good volume rendering
+    ))
+
+
+st.plotly_chart(fig)
